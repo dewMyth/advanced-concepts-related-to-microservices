9d723d088a74a6bbbcb89770f767ae22056b45d2
Dewmith Akalanka <akalankadewmith@gmail.com>
2024-08-06 20:16:17 +0530

    add swagger configuration & api docs

--- business-logic-service/src/main.ts
+++ business-logic-service/src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug', 'log', 'verbose'],
   });
+
+  // Swagger Configurations
+  const config = new DocumentBuilder()
+    .setTitle('Advanced Microservices Concepts')
+    .setDescription(
+      'This is a test application built by dewMyth for practicing several concepts that are used in microservices. Such as background job handling with bull queues',
+    )
+    .setVersion('1.0')
+    .addTag('')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
